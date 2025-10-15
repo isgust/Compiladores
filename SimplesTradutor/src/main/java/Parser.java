1274131e6b3165656b1810227d4efacd4a45ef7d
isgust <rocha.gustavops@gmail.com>
2025-10-15 06:09:27 -0300

    Refatorando o tradutor: incluindo o analisador léxico

--- SimplesTradutor/src/main/java/Parser.java
+++ SimplesTradutor/src/main/java/Parser.java
@@ -1,9 +1,59 @@
 public class Parser {
-    private byte[] input;
-    private int current;
+
+    private Scanner scan;
+    private char currentToken;
 
     public Parser (byte[] input) {
-        this.input = input;
+        scan = new Scanner(input);
+        currentToken = scan.nextToken();
     }
 
+    // Update token
+    private void nextToken () {
+        currentToken = scan.nextToken();
+    }
+
+    // Novo metodo match
+    private void match(char t) {
+        if (currentToken == t) {
+            nextToken();
+        }else {
+            throw new Error("syntax error");
+        }
+    }
+
+    // Novo metodo Digit
+    void digit () {
+        if (Character.isDigit(currentToken)) {
+            System.out.println("push " + currentToken);
+            match(currentToken);
+        } else {
+            throw new Error("syntax error");
+        }
+    }
+
+    // Novo metodo oper
+    void oper () {
+        if (currentToken == '+') {
+            match('+');
+            digit();
+            System.out.println("add");
+            oper();
+        } else if (currentToken == '-') {
+            match('-');
+            digit();
+            System.out.println("sub");
+            oper();
+        }
+    }
+
+    // Permanece o mesmo
+    void expr() {
+        digit();
+        oper();
+    }
+
+    public void parse () {
+        expr();
+    }
 }
